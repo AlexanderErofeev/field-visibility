afdce9e3e3b0d390d98f8401fc72cfd086a2c631
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-09-26 16:09:19 +0500

    Create graphene schema for users

--- core/schema.py
+++ core/schema.py
@@ -1,8 +1,13 @@
 import graphene
 import ingredients.schema
+import users.schema
 
 
-class Query(ingredients.schema.Query, graphene.ObjectType):
+class Query(
+    ingredients.schema.Query,
+    users.schema.Query,
+    graphene.ObjectType
+):
     pass
 
 
